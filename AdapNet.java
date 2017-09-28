8a6421e8adb1ad14d2c19a5c9e637131c14d4e23
maandrews <31116472+maandrews@users.noreply.github.com>
2017-09-28 17:56:25 -0400

    Update AdapNet.java

--- AdapNet.java
+++ AdapNet.java
@@ -5,7 +5,7 @@ import java.util.*;
  * This program uses evolutionary computation to simulate an adaptive network (graph) where its nodes are modelling a 
  * population vulnerable to disease.  Nodes may use different strategies to rewire their connections within the network
  * in order to avoid becoming infected.  The best strategies are replicated from generation to generation, in order to find
- * the optimal strategy that minimizes time infected.
+ * the optimal strategy that maximizes influence in the network.
  */
 
 public class AdapNet {
